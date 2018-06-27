e6c226bbb31f60c5db0f97da6b37984e575d5fc9
sco7 <sco77.townson@gmail.com>
2018-06-27 12:49:57 +0100

    refactored listen file for heroku deployment

--- listen.js
+++ listen.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-console */
 
+// for heroku deployment
+
 const app = require('./server');
-const PORT = 3001;
+const PORT = process.env.NODE_ENV === 'production'? process.env.PORT : 3001;
 
 app.listen(PORT, function(err) {
   if (err) return console.log(err);
   console.log(`App listening on port ${PORT}...`);
 });
-
-// for heroku deployment
-if (process.env.NODE_ENV === 'production') {PORT = process.env.PORT;}
